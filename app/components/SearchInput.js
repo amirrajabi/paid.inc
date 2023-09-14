6610057abd1cb2910e6c0445998048f4a283af7b
amirrajabi <amir.divergent@gmail.com>
2023-09-14 17:22:38 +1000

    fix: resolved debugging issue for incorrect data retrieval

--- app/components/SearchInput.js
+++ app/components/SearchInput.js
@@ -22,7 +22,7 @@ export const SearchInput = ({ fetchNewsData }) => {
     };
 
     return (
-        <View className="h-28">
+        <View className="h-32 items-center">
             <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4 w-80">
                 <View className="flex-1 flex-row space-x-2 bg-slate-100 p-3 rounded-md rounded-3xl">
                     <TouchableOpacity onPress={handleReturnPress}>
@@ -40,15 +40,19 @@ export const SearchInput = ({ fetchNewsData }) => {
                     />
                 </View>
             </View>
-            <ScrollView horizontal={true} className="mt-2">
-                {searchHistory.map((item, index) => (
-                    <TouchableOpacity key={index} onPress={() => handleHistoryClick(item)}>
-                        <View className="bg-blue-400 rounded-full p-3 mr-2">
-                            <Text className="text-white font-semibold">{item}</Text>
-                        </View>
-                    </TouchableOpacity>
-                ))}
-            </ScrollView>
+            {searchHistory?.length !== 0 && (
+                <View className="w-full">
+                    <ScrollView horizontal={true} className="w-96 pl-2 mt-2" showsHorizontalScrollIndicator={false}>
+                        {searchHistory.map((item, index) => (
+                            <TouchableOpacity key={index} onPress={() => handleHistoryClick(item)}>
+                                <View className="bg-blue-400 rounded-full p-3 mr-2">
+                                    <Text className="text-white font-semibold">{item}</Text>
+                                </View>
+                            </TouchableOpacity>
+                        ))}
+                    </ScrollView>
+                </View>
+            )}
         </View>
     );
 }
